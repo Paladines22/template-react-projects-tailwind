4d123a147d498cdaf4345bdf325b7c41aa88ea62
ValentinaPaladines <val.paladinezunal@gmail.com>
2023-07-12 08:12:43 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="flex justify-center text-2xl font-bold">React Template</h1>
+    </div>
+  );
+};
+
+export default App;
